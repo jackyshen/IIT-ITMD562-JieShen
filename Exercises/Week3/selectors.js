5017bebb64d815b723daf4c9692b6a35d7892d94
Jie Shen <shenjie7810@gmail.com>
2017-09-12 17:01:54 -0500

    selectors.js

--- Exercises/Week3/selectors.js
+++ Exercises/Week3/selectors.js
@@ -1,16 +1,13 @@
-
-var main =function(){
-
+var main =function()
+{
 	var font_colors = ['red','orange','yellow','green','cyan','blue','purple'];
-
-	$('.relevant p').hide();
-
-	for (i = 0; i < 7; i++) {
-		var num = $(this).find('.relevant p:nth-child('+(i+1)+')');
+  $("p").hide();
+	for (i = 0; i < 7; i++) 
+	{
+	var num = $(this).find('p:nth-child('+(i+1)+')');
 		num.css('color', font_colors[i]);
-		num.delay(i*400).fadeIn(1200);
+		num.delay(i*300).fadeIn(1000);
 	}
 };
-
 $(document).ready(main);
 
